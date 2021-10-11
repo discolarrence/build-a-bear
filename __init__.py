1c57ce4e1dd3732676c61aa86472251280f56109
discolarrence <discolarrence@gmail.com>
2021-10-10 23:50:00 -0400

    added cookies & background color

--- __init__.py
+++ __init__.py
@@ -6,3 +6,5 @@ from flask import Flask
 app = Flask(__name__)
 
 import build_a_bear.views
+import build_a_bear.options
+
--- views.py
+++ views.py
@@ -2,15 +2,34 @@
 Routes and views for the flask application.
 """
 
-from datetime import datetime
-from flask import render_template, redirect, url_for, request
+import json
+from flask import render_template, redirect, url_for, request, make_response
 from build_a_bear import app
+from build_a_bear.options import DEFAULTS
 
+
+def get_saved_data():
+    try:
+        data = json.loads(request.cookies.get('character'))
+    except TypeError:
+        data = {}
+    return data
+
+ 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', saves=get_saved_data())
+
+
+@app.route('/builder')
+def builder():
+    return render_template('builder.html', saves=get_saved_data(), options=DEFAULTS)
+
 
 @app.route('/save', methods=['POST'])
 def save():
-    import pdb; pdb.set_trace()
-    return redirect(url_for('index'))
+    response = make_response(redirect(url_for('builder')))
+    data = get_saved_data()
+    data.update(dict(request.form.items()))
+    response.set_cookie('character', json.dumps(data))
+    return response
